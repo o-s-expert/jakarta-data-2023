30762b0dcaaf5744fb90db2db47943d538fc0aeb
Otavio Santana <otaviopolianasantana@gmail.com>
2023-07-26 19:28:00 +0100

    feat: create BeerTemplate resource sample
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ document-nosql/src/main/java/os/expert/examples/BeerTemplateResource.java
@@ -0,0 +1,77 @@
+package os.expert.examples;
+
+
+import com.github.javafaker.Faker;
+import jakarta.data.repository.Sort;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.nosql.document.DocumentTemplate;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+
+import java.util.List;
+
+@ApplicationScoped
+@Path("beers2")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class BeerTemplateResource {
+
+    private final DocumentTemplate template;
+
+    @Inject
+    public BeerTemplateResource(DocumentTemplate template) {
+        this.template = template;
+    }
+
+    @Deprecated
+    BeerTemplateResource() {
+        this(null);
+    }
+
+
+    @GET
+    public List<Beer> findByAll(@BeanParam BeerParam param){
+
+        if(param.isMaltAndHopQuery()){
+           return this.template.select(Beer.class).where("malt")
+                    .eq(param.malt())
+                    .and("hop")
+                    .eq(param.hop())
+                    .result();
+        }
+        else if(param.isHopQuery()) {
+            return this.template.select(Beer.class).where("hop")
+                    .eq(param.hop())
+                    .result();
+        }
+        else if(param.isMaltQuery()) {
+            return this.template.select(Beer.class).where("malt")
+                    .eq(param.malt())
+                    .result();
+        }
+        return this.template.select(Beer.class).result();
+    }
+
+    @POST
+    public void create(Beer beer){
+        this.template.insert(beer);
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void deleteById(@PathParam("id") String id){
+        this.template.delete(Beer.class).where("id").eq(id).execute();
+    }
+
+    @Path("random")
+    @POST
+    public void random() {
+        var faker = new Faker();
+        for (int index = 0; index < 1_000; index++) {
+            var beer = Beer.of(faker);
+            this.template.insert(beer);
+        }
+    }
+
+}
